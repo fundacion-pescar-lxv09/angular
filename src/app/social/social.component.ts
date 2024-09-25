aab70e53539aab70530a634f9c17c90444445274
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-25 10:02:43 -0300

    crear componente redes sociales utilizando commonmodule

---
+++ src/app/social/social.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+interface Link {
+  text: string,
+  link: string,
+  icon?: string
+}
+@Component({
+  selector: 'app-social',
+  standalone: true,
+  imports: [ CommonModule ],
+  templateUrl: './social.component.html',
+  styleUrl: './social.component.css'
+})
+export class SocialComponent {
+  phone?: string = "54110303456"
+  message?: string = ""
+  links:Link[] = [
+    { 
+      text:"LinkedIn",
+      link:"https://linkedin.com/in/profile",
+    },
+    { 
+      text:"Whatsapp",
+      link:`https://api.whatsapp.com/send?phone=${this.phone}&text=${this.message}`,
+    },
+    { 
+      text:"GitHub",
+      link:"https://github.com/fundacion-pescar-lxv09/angular",
+    },
+  ]
+}
