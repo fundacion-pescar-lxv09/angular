228614af5445b576e8bd5639c9cdad1caf5f1b73
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-25 01:42:41 -0300

    crear componente header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  title = "Fundacion Pescar"
+  subtitle="Capacitacion Angular 2"
+}
