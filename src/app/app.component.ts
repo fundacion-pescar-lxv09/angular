0b563dcfe5d87a6c620d35e67162fb0c5e81f078
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-25 11:57:25 -0300

    configurar rutas de aplicacion y estructura principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -14,13 +14,15 @@ import { NavComponent } from "./nav/nav.component";
         FooterComponent,
     ],
     template: `
-        <app-nav/>
+        <app-nav [appname]="title"/>
         <app-header [title]="title" [subtitle]="subtitle"/>
-        <app-footer/>
-        `,
+        <main>
+            <router-outlet></router-outlet>
+        </main>
+        <app-footer/>`
 })
-
 export class AppComponent{
     title = "Fundacion Pescar"
     subtitle = "Capacitacion Angular 2"
+    current = ""
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../routes/home/home.component';
+import { LoginComponent } from '../routes/login/login.component';
+import { SigninComponent } from '../routes/signin/signin.component';
+import { ChartComponent } from '../routes/chart/chart.component';
+import { ProductsComponent } from '../routes/products/products.component';
+import { ContactComponent } from '../routes/contact/contact.component';
+import { NotFoundComponent } from '../routes/not-found/not-found.component';
 
 export const routes: Routes = [
-
+    { path: '', redirectTo: '/home', pathMatch: "full" },
+    { path: 'home',  component:HomeComponent},
+    { path: 'login', component:LoginComponent},
+    { path: 'signin', component:SigninComponent},
+    { path: 'chart', component:ChartComponent},
+    { path: 'products', component:ProductsComponent},
+    { path: 'contact', component:ContactComponent},
+    { path: '*', component:NotFoundComponent}
 ];
