5066071459ad3ee8730319dd468ae1b0c3e18d08
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-27 11:01:03 -0300

    formulario con plantilla de inicio de sesion

--- src/routes/login/login.component.ts
+++ src/routes/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe, UpperCasePipe } from '@angular/common';
 
+interface User{
+  username: string,
+  password: string
+}
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, JsonPipe, UpperCasePipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @Output() onlogin = new EventEmitter<User>()
+  text = "iniciar sesion"
+  show = false;
+  login = {
+    username: 'c215714n',
+    password: ''
+  }
+  alert = {
+    code: 0,
+    title: "Inicio de Session",
+    message: "Estamos verificando los datos, podras acceder al sition en unos segundos... Muchas Gracias por Ingresar"
+  }
 
+  userLogin(){
+    this.onlogin.emit(this.login)
+    this.show = true;
+  }
+  closeAlert(){
+    this.show = false;
+  }
 }
