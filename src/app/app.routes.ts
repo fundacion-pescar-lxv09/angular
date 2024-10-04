ba3622c5fe4d2bb0a9f1c92ee3ac31413349bf10
Cristian Racedo <cristiandracedo@hotmail.com>
2024-10-04 09:02:12 -0300

    actualizar rutas de aplicacion y navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CandidatesComponent } from './routes/candidates/candidates.component';
+import { AboutComponent } from './routes/about/about.component';
+import { ConfigComponent } from './routes/config/config.component';
+import { SelectComponent } from './routes/select/select.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'home', pathMatch:'full' },
+    {path: 'home', component: CandidatesComponent },
+    {path: 'about', component: AboutComponent },
+    {path: 'config', component: ConfigComponent },
+    {path: 'selection', component: SelectComponent },
+    {path: '**', redirectTo: 'home'},
+];
