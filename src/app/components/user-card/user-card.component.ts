eca39f3faf411053c67c89749e55af23d05acbf6
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-06 13:09:54 -0300

    remover verificaciones e implementar provider en user-card

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -5,6 +5,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink } from '@angular/router';
+import { UserData } from '../../providers/user.interface';
+import { SelectService } from '../../providers/select.service';
 
 @Component({
   selector: 'app-user-card',
@@ -17,14 +19,26 @@ import { RouterLink } from '@angular/router';
     MatButtonModule,
     MatButtonToggle,
   ],
+  providers: [SelectService],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css'
 })
 export class UserCardComponent {
-  @Input() cand:any
+  @Input() cand:UserData|any = {}
+  chosen = false
   dataList = [
     { prop: "tel:+", key: "phone", text: "Telefono"},
     { prop: "tel:+", key: "cell",  text: "Celular"},
     { prop: "mailto:", key: "email", text: "Correo"},
   ]
+  constructor(private select:SelectService){}
+  selectUser(){
+    this.chosen ?
+    this.select.removeUser(this.cand.login.uuid):
+    this.select.addUser(this.cand)
+      
+    this.chosen = !this.chosen
+
+    this.select.getUsers()
+  }
 }
